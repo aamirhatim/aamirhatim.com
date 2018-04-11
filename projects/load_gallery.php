03ee0f0364b20e93f8be00d05ab6bacfe6c923c1
Aamir Husain <aamir300@gmail.com>
2018-04-10 20:04:17 -0500

    Do not show gallery if no picures are listed in project_info.txt

--- projects/load_gallery.php
+++ projects/load_gallery.php
@@ -1,13 +1,19 @@
 <?php
 echo '<div id = "gallery">';
-echo '<div id = "large-thumb">';
-foreach ($large_thumb as $key) {
-  echo '<img src = "img/thumb/'.$key.'">';
+if (trim($large_thumb[0]) != 'none') {
+  echo '<div id = "large-thumb">';
+  foreach ($large_thumb as $key) {
+    echo '<img src = "img/thumb/'.$key.'">';
+  }
+  echo '</div>';
+}
+
+if (trim($small_thumb[0]) != 'none') {
+  echo '<div id = "small-thumb">';
+  foreach ($small_thumb as $key) {
+    echo '<img src = "img/thumb/'.$key.'">';
+  }
+  echo '</div>';
 }
 echo '</div>';
-echo '<div id = "small-thumb">';
-foreach ($small_thumb as $key) {
-  echo '<img src = "img/thumb/'.$key.'">';
-}
-echo '</div></div>';
 ?>
