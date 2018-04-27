9c71d28e749ae37eb9fb50fddfd519ef35a2c6fe
Aamir Husain <aamir300@gmail.com>
2018-04-27 00:35:53 -0500

    Add college tiles

--- navLinks/about/about.php
+++ navLinks/about/about.php
@@ -31,6 +31,27 @@
           <p>
             <?php echo $bio; ?>
           </p>
+
+          <div id = "schools">
+            <div class = "school-tile">
+              <div class = "school-txt">
+                <h1>Master of Science</h1>
+                <h2>Robotics</h2>
+                <img src = "/img/wildcat.svg">
+                <h2>Northwestern University</h2>
+              </div>
+              <div class = "school-hover" style = "background-color: #4E2A84">GO CATS!</div>
+            </div>
+            <div class = "school-tile">
+              <div class = "school-txt">
+                <h1>Bachelor of Science</h1>
+                <h2>Biomedical Engineering</h2>
+                <img src = "/img/colonial.svg">
+                <h2>George Washington University</h2>
+              </div>
+              <div class = "school-hover" style = "background-color: #004065">RAISE HIGH!</div>
+            </div>
+          </div>
         </div>
       </div>
     </main>
