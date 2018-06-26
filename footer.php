6e2e914224a9051c07e85e915cd248c95e56ea27
Aamir Husain <aamir300@gmail.com>
2018-06-25 21:02:43 -0500

    Edit footer

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <address>
-  Copywrite © Aamir Husain ||| Like this theme? Get it <a style = "color: white;" href = "https://github.com/aamirhatim/website_template.git" target = "blank">here</a>!
+  Copyright © Aamir Husain ||| Like this theme? Get it <a style = "color: white;" href = "https://github.com/aamirhatim/website_template.git" target = "blank">here</a>!
 </address>
