3d0f64827392c7a56044ad32e5c4cc5f652e5740
Aamir Husain <aamir300@gmail.com>
2019-01-09 19:38:42 -0500

    Edit css and javascript

--- js/fades.js
+++ js/fades.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
   $(".project").each(function() {
     if ($(this).hasClass('highlight')) {
       $(this).delay(delay_time)
-      .animate({opacity: [1, "swing"]}, 1500);
-      delay_time += 150;
+      .animate({opacity: [1, "swing"]}, 500);
+      delay_time += 60;
     }
     else {
       $(this).hide();
