a669f13499473301763779758821c289797a4fa4
Aamir Husain <aamir300@gmail.com>
2019-08-14 22:10:43 -0400

    Add landing page

--- v6/home.php
+++ v6/home.php
@@ -1,1 +1,15 @@
-<h1>Hello</h1>
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Aamir Husain</title>
+    <link rel='stylesheet' href='css/global.css'>
+</head>
+
+<body>
+    <!-- Landing Page -->
+    <?php require 'modules/landing.html';?>
+</body>
+
+</html>
