c333ead12087a87fe4056492ffa9a5a494f4e47b
Aamir Husain <aamir300@gmail.com>
2019-08-15 17:09:47 -0400

    create accordion panels for projects

--- v6/js/project_tiles.js
+++ v6/js/project_tiles.js
@@ -2,7 +2,7 @@ function fill_template(data) {
     var t, item;
 
     // Get template
-    t = document.querySelector('#tile-template').content;
+    t = document.querySelector('#panel-template').content;
 
     // Populate template
     node = document.importNode(t, true);
@@ -11,7 +11,7 @@ function fill_template(data) {
     node.querySelector('.proj-text').textContent = data['description'];
 
     // Activate template
-    document.querySelector('#tiles').appendChild(node);
+    document.querySelector('#accordion').appendChild(node);
 };
 
 function load_projects() {
@@ -31,3 +31,29 @@ function load_projects() {
 };
 
 document.addEventListener('DOMContentLoaded', load_projects());
+
+document.addEventListener('DOMContentLoaded', function() {
+    var base = document.getElementById('accordion');
+    var selected_proj;
+    if (base) {
+        base.addEventListener('click', function(event) {
+            console.log(event.target.classList);
+            if (event.target.classList.contains('panel-active')) {
+                event.target.style.flexGrow = '1';
+                event.target.classList.remove('panel-active');
+            } else {
+                event.target.style.flexGrow = '20';
+                event.target.classList.add('panel-active');
+            }
+
+            if (selected_proj) {
+                if (selected_proj != event.target) {
+                    selected_proj.style.flexGrow = '1';
+                    selected_proj.classList.remove('panel-active');
+                }
+            }
+
+            selected_proj = event.target;
+        });
+    }
+})
