4e7909cecdab23d5aec4519be3aeec28dd8853ce
Aamir Husain <aamir300@gmail.com>
2019-08-28 03:27:18 -0400

    rearrange sections

--- v6/home.php
+++ v6/home.php
@@ -5,15 +5,16 @@
 <head>
     <title>Aamir Husain</title>
     <link rel='stylesheet' href='/v6/css/global.css'>
+    <link href="https://fonts.googleapis.com/css?family=Hepta+Slab|Open+Sans|Titillium+Web|Roboto+Mono:300&display=swap" rel="stylesheet">
 </head>
-
 <body>
     <?php
     
     require 'modules/nav.html';         // Navigation
     require 'modules/landing.html';     // Landing page
-    require 'modules/projects.html';    // Projects
     require 'modules/about.html';       // About
+    require 'modules/skills.html';      // Skills
+    require 'modules/projects.html';    // Projects
     require 'modules/footer.html';      // Footer
 
     ?>
