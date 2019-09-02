600621e38b7bbf1aadef8be91bca8c4688f750f5
Aamir Husain <aamir300@gmail.com>
2019-09-02 19:06:01 -0400

    Add href anchors

--- v6/modules/nav.php
+++ v6/modules/nav.php
@@ -3,8 +3,8 @@
 </head>
 
 <nav>
-    <a id='nav-skills' class='nav-link'><?php echo file_get_contents('img/nav_skills.svg'); ?></a>
-    <a id='nav-projects' class='nav-link'><?php echo file_get_contents('img/nav_projects.svg'); ?></a>
-    <a id='nav-about' class='nav-link'><?php echo file_get_contents('img/nav_about.svg'); ?></a>
-    <a id='nav-resume' class='nav-link'><?php echo file_get_contents('img/nav_resume.svg'); ?></a>
+    <a id='nav-skills' class='nav-link' href='#skills'><?php echo file_get_contents('img/nav_skills.svg'); ?></a>
+    <a id='nav-projects' class='nav-link' href='#projects'><?php echo file_get_contents('img/nav_projects.svg'); ?></a>
+    <a id='nav-about' class='nav-link' href='#about'><?php echo file_get_contents('img/nav_about.svg'); ?></a>
+    <a id='nav-resume' class='nav-link' href='footer'><?php echo file_get_contents('img/nav_resume.svg'); ?></a>
 </nav>
