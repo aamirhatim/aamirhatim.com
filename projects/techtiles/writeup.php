19b52a3b60f0dd9f9c12f4573939dafcdcc13ccd
Aamir Husain <aamir300@gmail.com>
2019-09-02 22:13:56 -0400

    add projects to root projects folder

---
+++ projects/techtiles/writeup.php
@@ -0,0 +1,88 @@
+<h4>Overview</h4>
+<p>
+  This project was completed as a capstone senior design project at The George Washington University, winning second place and awarded for being the "most marketable project" at the GW Pelton Award Competition in 2015.
+</p><p>
+  The goal of Techtiles was to provide users the option of a more comprehensive system for tracking their biometrics through a mobile app. We also wanted to introduce wearable technology that can be completely washable without having to remove all the electronics. We envisioned this device being useful in a variety of applications including firefighting, space travel, and athletics. Techtiles uses three sensors to gather all of its data: an accelerometer, electrocardiogram (ECG), and a stretch sensor. With these, the device can extract useful information like heart rate, breathing rate, steps taken, distance traveled, and energy expended.
+</p><p>
+  The project was divided into three main categories: Hardware, Data Processing, and App Development.
+</p>
+
+<h4>Hardware & Design</h4>
+<p>
+  The Hardware subsystem included choosing the fabric for the shirt (as well as any stitching), designing the analog filters used for the ECG and breathing sensor, building a custom circuit board, and designing a housing for the non-washable electronic components. The final design for the device is the sleeveless, moisture wicking compression shirt shown below with its electronic housing attachment.
+</p>
+
+<div class = "info-box" style = "width: 70%;">
+  <h6>Sensors Used</h6>
+  <br>
+  <table class = "info-box-table">
+    <tr>
+      <td><img src = "/img/accelerometer.svg"></td>
+      <td><img src = "/img/ecg.svg"></td>
+      <td><img src = "/img/resistor.svg"></td>
+    </tr>
+    <tr>
+      <td>Accelerometer</td>
+      <td>ECG</td>
+      <td>Fabric Stretch Sensor</td>
+    </tr>
+  </table>
+  <br><br>
+
+  <h6>Extracted Data</h6>
+  <br>
+  <table class = "info-box-table">
+    <tr>
+      <td><img src = "/img/heart.svg"></td>
+      <td><img src = "/img/lungs.svg"></td>
+      <td><img src = "/img/steps.svg"></td>
+      <td><img src = "/img/distance.svg"></td>
+      <td><img src = "/img/flame.svg"></td>
+    </tr>
+    <tr>
+      <td>Heart Rate</td>
+      <td>Breathing Rate</td>
+      <td>Steps</td>
+      <td>Distance</td>
+      <td>Energy Expended</td>
+    </tr>
+  </table>
+</div>
+
+<figure>
+  <img src = "/img/hardware.png" width = "400px">
+  <figcaption>
+    (Top) Compression shirt with respiration sensors and ECG electrodes. (Bottom) Final PCB with Arduino Due and Bluetooth shield connected, placed inside 3D printed housing.
+  </figcaption>
+</figure>
+
+<p>
+  The shirt is equipped with two bands of silver coated nylon along the left and right sides of the torso which serve as stretch resistors that measure breathing. This special conductive fabric is washable and stretchable in all directions, making it an excellent choice for the sensor design. A low pass filter was designed to only allow the lower frequencies associated with breathing to pass through.
+</p><p>
+  The ECG used a basic 2-lead configuration and a ground. Anything more complex, although more accurate, reduced the durability of the device and also provided more information that was needed for this project. The same fabric used for the respiration sensor was used for the ECG electrodes as well, which were made up of three square pieces stitched underneath the shirt.
+</p><p>
+  Movement was detected using a 3-axis accelerometer. With this data, a program was written to detect peaks of increased g force, indicating a step. By counting these steps as well as their frequency, distance traveled and energy expended could be calculated in real time as well.
+</p><p>
+  The brain that connected all the components together was the Arduino Pro Mini (seen above). It was chosen for its relatively low power consumption (3.3V) and the availability of analog and digital pins.
+</p>
+
+<h4>Data Processing</h4>
+<p>
+  Before measuring any real data, an initial calibration phase is run to fine tune any parameters specific to the user such as gait, height, weight, and respiration volume. Once calibration is complete, the Arduino runs averaging and smoothing filters on the incoming data to clean it up more. Since heart rate, steps, and breathing are counted by observing maximum values of a rhythmic data set, this cleaned data is examined for any sharp peaks in the waveform. From here, heart and respiration rate is found by simply dividing the number of peaks counted by the elapsed time. Furthermore, distance is determined by multiplying the number of steps counted by the user's gait, and energy expended is calculated by using the user's weight and their speed (determined by dividing the distance traveled by the elapsed time).
+</p>
+
+<figure>
+  <img src = "/img/respiration.png" width = "400px">
+  <figcaption>
+    Sample data for respiration. As the user breathes in, the resitance of the sensor changes, resulting in the waveform above. The observed peaks were used in determining repiration rate.
+  </figcaption>
+</figure>
+
+<h4>Mobile Display</h4>
+<p>
+  To view the data, a user-friendly Android app was devloped to recieve output biometrics from the shirt via Bluetooth. Using the “android.Bluetooth” API in Android Studio, pairing with the shirt is immediately initiated once the app is opened.
+</p>
+
+<figure>
+  <img src = "/img/phone.svg" width = "200px">
+</figure>
---
+++ projects/website/writeup.php
@@ -0,0 +1,31 @@
+<h4>Overview</h4>
+<p>
+  What started out as a curiosity project soon became what I thought was a necessity. Rather than updating a paper resume and periodically throwing it at whoever may glance my way, building a website could instead serve as a dynamic resume where I can show my skills and creativity through a brand new medium.
+</p><p>
+  I debated using pre-made templates for their ease of use and clean looks on different sized screens, but I had a desire to learn the basics of web development from the ground up. I quickly became hooked, spiking my interests in graphic design and aesthetics of digital art.
+</p>
+
+<h4>Current Version</h4>
+<h6>Version 5 - Getting Cozy with PHP</h6>
+<p>
+  This version is clean on the inside and out. Previous versions of my site were not modular whatsoever. Redundant code, different standards, it was hard to maintain. For Version 5 I wanted to change that. I made extensive use of PHP, plugging in reusable code via server-side commands rather than re-typing it for every new page. I also simplified adding new pages by setting up text file parsing for an overall easier experience rather than unnecessarily getting too deep in the weeds of HTML.
+</p>
+
+<h4>Previous Versions</h4>
+<p>
+  Every once in a while I decide to scrap my current design and start from scratch again. Every time I learn something new, and I refine my tastes in web design. Check out previous versions of my site below.
+</p><p>
+  NOTE: These versions are no longer maintained, so there may be some parts and features missing!
+</p>
+
+<div class = "info-box">
+  <ul>
+    <li><a href = "../../v4/v4.html" target = "blank">Version 4: Vibrant colors and subtle scrolling effects</a></li>
+    <br>
+    <li><a href = "../../v3/tile.html" target = "blank">Version 3: Experimenting with tiled layouts</a></li>
+    <br>
+    <li><a href = "../../v2/v2.html" target = "blank">Version 2: A quirky one-pager with sliding cards</a></li>
+    <br>
+    <li><a href = "../../v1/v1.php" target = "blank">Version 1: A single-scroll page using PHP</a></li>
+  </ul>
+</div>
